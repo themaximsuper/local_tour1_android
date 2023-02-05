1f176d20116f7f540429841f9d129acd98d25783
themaximsuper <malinkovichmax@gmail.com>
2023-02-05 19:24:39 +0300

    1

--- app/src/main/java/com/example/tournament1/MainActivity.kt
+++ app/src/main/java/com/example/tournament1/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.tournament1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.calendar_c_view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        months.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        days.layoutManager = GridLayoutManager(this, 7)
     }
 }
