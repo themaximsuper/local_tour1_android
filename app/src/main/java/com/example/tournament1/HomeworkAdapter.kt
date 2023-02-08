9907793e7985961d6079828c3b27d79c3730bfe5
themaximsuper <malinkovichmax@gmail.com>
2023-02-08 21:14:15 +0300

    1

--- app/src/main/java/com/example/tournament1/HomeworkAdapter.kt
+++ app/src/main/java/com/example/tournament1/HomeworkAdapter.kt
@@ -1,14 +1,16 @@
 package com.example.tournament1
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.homework_item.view.*
 
 class HomeworkViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
 
 class HomeworkAdapter (val tasks: List<Task>) : RecyclerView.Adapter<HomeworkViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeworkViewHolder {
-        TODO("Not yet implemented")
+        return HomeworkViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.homework_item, parent, false))
     }
 
     override fun getItemCount(): Int {
@@ -16,6 +18,6 @@ class HomeworkAdapter (val tasks: List<Task>) : RecyclerView.Adapter<HomeworkVie
     }
 
     override fun onBindViewHolder(holder: HomeworkViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.itemView.tesk_text.text = tasks[position].title
     }
 }
